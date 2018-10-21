45ce7deda868f16536be02920d95ea507e26277f
Robert Ball <rob@coder73.com>
2018-10-21 16:19:39 +0100

    basic animation for disc drop & next disc image

--- app/src/main/java/com/coder73/connectfour/MainActivity.java
+++ app/src/main/java/com/coder73/connectfour/MainActivity.java
@@ -3,16 +3,30 @@ package com.coder73.connectfour;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends Activity implements GameListener {
     Game _game = new Game(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setNextDiscImageView();
+    }
+
+    private void setNextDiscImageView() {
+        ImageView nextDiscImageView = findViewById(R.id.nextDiscImageView);
+
+        if(_game._disc == Disc.RED) {
+         nextDiscImageView.setImageResource(R.drawable.red);
+        }
+        else {
+            nextDiscImageView.setImageResource(R.drawable.yellow);
+        }
     }
 
     @Override
@@ -28,8 +42,22 @@ public class MainActivity extends Activity implements GameListener {
 
     public void playButtonOnClick(View view) {
         Random random = new Random();
+        ImageView imageView = (ImageView) view;
+        imageView.setTranslationY(-1000f);
+
+        if(_game._disc == Disc.YELLOW) {
+            imageView.setImageResource(R.drawable.yellow);
+        }
+        else {
+            imageView.setImageResource(R.drawable.red);
+        }
+
+        imageView.animate().translationYBy(1000f).setDuration(300);
+
         int col = random.nextInt((6 - 0) + 1) + 0;
         Toast.makeText(this, String.format("%s Playing column %d", _game.getDisc().toString(), col), Toast.LENGTH_SHORT).show();
+
         _game.play(col);
+        setNextDiscImageView();
     }
 }
